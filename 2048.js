272e85e651388fb72682046f08436bd8cca574c2
Hugo Rafael <98796954+hugorafaelll@users.noreply.github.com>
2022-07-08 14:52:43 -0300

    slide function left

--- 2048.js
+++ 2048.js
@@ -53,3 +53,41 @@ document.addEventListener("keyup", (e)=>{ // keyup e quando terminamos de aperta
         slideLeft();
     }
 })
+
+function filterZero(row){
+    return row.filter(num => num !=0)  //cria um novo array sem os valores zero 
+}
+
+function slide(row) {
+    //[0,2,2,2]
+    row = filterZero(row);
+
+    //slide   onde a magica acontece
+    for (let i=0; i < row.length ; i++){
+        //verifica cada linha 
+        if(row[i] == row[i+1]){ // se a linah for igual a linha do lado 
+            row[i]*2;  // multiplica o numero por 2  "merge"
+            row[i+1] = 0; // o numero do lado e igual a zero 
+            score += row[i] //acrecenta valor no score
+        }
+    }
+    row = filterZero(row)   //[4,2]
+    while (row.length <colums) {//adiciona os zeros nos campos vazios 
+        row.push(0);    //[4,2,0,0]
+    }
+    return row;
+}
+
+function slideLeft(){
+    for (let r= 0; r<rows; r++){
+        let row = board[r];
+        row = slide(row);
+        board[r] =row;
+
+        for ( let c =0; c < colums ; c++){
+            let tile = document.getElementById(r.toString() + "-" + c.toString());
+            let num = board [r][c];
+            updateTile(tile,num);
+        }
+    }
+};
